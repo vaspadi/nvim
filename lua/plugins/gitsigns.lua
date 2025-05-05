3e0319acd432145609f0f382ffe9f537a3bd44de
vaspadi <vasiliev.work1@gmail.com>
2025-05-05 22:37:21 +0300

    feat(gitsigns): add keymaps

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,5 @@
 return {
 	"lewis6991/gitsigns.nvim",
-	branch = "fix/issue1300",
 	config = function()
 		-- Удалить знаки диагностики, чтобы не скрывали gitsigns
 		vim.diagnostic.config({
@@ -10,27 +9,82 @@ return {
 		require("gitsigns").setup({
 			current_line_blame = true,
 			signs = {
-				-- add = { text = "┃" },
-				-- change = { text = "┃" },
 				delete = { text = "◣" },
 				topdelete = { text = "◤" },
 				changedelete = { text = "┃" },
-				-- untracked = { text = "┆" },
 			},
-			-- signs = {
-			--   delete = { text = '▶' },        -- или '▸'
-			--   topdelete = { text = '▶' },
-			--   changedelete = { text = '▶' },
-			-- }
 			signs_staged = {
-				-- add = { text = "┃" },
-				-- change = { text = "┃" },
 				delete = { text = "◣" },
 				topdelete = { text = "◤" },
 				changedelete = { text = "┃" },
-				-- untracked = { text = "┆" },
 			},
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
+
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
+
+				-- Navigation
+				map("n", "gh", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.nav_hunk("next")
+					end
+				end)
+
+				map("n", "gH", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
+					end
+				end)
+
+				-- Actions
+				-- git add
+				map("n", "<leader>hs", gitsigns.stage_hunk)
+				map("v", "<leader>hs", function()
+					gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end)
+
+				-- git reset
+				map("n", "<leader>hr", gitsigns.reset_hunk)
+				map("v", "<leader>hr", function()
+					gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end)
+
+				map("n", "<leader>hS", gitsigns.stage_buffer)
+				map("n", "<leader>hR", gitsigns.reset_buffer)
+				map("n", "<leader>hp", gitsigns.preview_hunk)
+				map("n", "<leader>hi", gitsigns.preview_hunk_inline)
+
+				map("n", "<leader>hb", function()
+					gitsigns.blame_line({ full = true })
+				end)
+
+				map("n", "<leader>hd", gitsigns.diffthis)
+
+				map("n", "<leader>hD", function()
+					gitsigns.diffthis("~")
+				end)
+
+				map("n", "<leader>hQ", function()
+					gitsigns.setqflist("all")
+				end)
+				map("n", "<leader>hq", gitsigns.setqflist)
+
+				-- Toggles
+				map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+				map("n", "<leader>tw", gitsigns.toggle_word_diff)
+
+				-- Text object
+				map({ "o", "x" }, "ih", gitsigns.select_hunk)
+			end,
 		})
-		-- require("scrollbar.handlers.gitsigns").setup()
+		require("scrollbar.handlers.gitsigns").setup()
 	end,
 }
