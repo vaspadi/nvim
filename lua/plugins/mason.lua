bf615b0fd4ee888224d903acc69ec19315eaa254
vaspadi <vasiliev.work1@gmail.com>
2025-05-07 23:03:27 +0300

    fix(mason): use v1.*

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,17 +1,19 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate",
-    config = true,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" },
-        automatic_installation = true,
-      })
-    end,
-  },
+	{
+		"mason-org/mason.nvim",
+		version = "1.*",
+		build = ":MasonUpdate",
+		config = true,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		version = "1.*",
+		dependencies = { "mason-org/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" },
+				automatic_installation = true,
+			})
+		end,
+	},
 }
